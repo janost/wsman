b2510080007ed09e5b0c1723089d33f516dd7497
janost <janost@users.noreply.github.com>
2019-04-05 19:22:24 +0200

    Add setup, rename generate to setup_all

--- src/cli.cr
+++ src/cli.cr
@@ -11,23 +11,42 @@ module Wsman
       run do |opts, args|
         puts opts.help_string
       end
-      sub "generate" do
+      sub "setup_all" do
         desc "Generate site configurations."
-        usage "new [options] <domain>"
+        usage "setup_all"
         run do |opts, args|
           log = Logger.new(STDOUT)
           handler = Wsman::Handler.new
           handler.prepare_env
-          log.info("Found sites: " + handler.site_manager.names.join(", "))
+          log.info("Processing sites: " + handler.site_manager.names.join(", "))
           sites = handler.site_manager.sites
           if sites.size == 0
             log.info("No sites found. Exiting...")
             exit 0
           end
           sites.each do |site|
-            handler.process_site(site)            
+            handler.process_site(site)
           end
-          handler.post_process          
+          handler.post_process
+        end
+      end
+      sub "setup" do
+        desc "Generate site configurations."
+        usage "setup [options] <sitename>"
+        run do |opts, args|
+          log = Logger.new(STDOUT)
+          if args.size == 0
+            log.info("Please list sites to process.")
+            Process.exit(0)
+          end
+          handler = Wsman::Handler.new
+          handler.prepare_env
+          handler.site_manager.sites.each do |site|
+            if args.includes? site.site_name
+              handler.process_site(site)
+            end
+          end
+          handler.post_process
         end
       end
     end
